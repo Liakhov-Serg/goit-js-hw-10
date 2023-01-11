4b5c1248feceb1bef6b52e1d47441c07592ee10d
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-11 18:17:09 +0000

    Deploying to gh-pages from @ Liakhov-Serg/goit-js-hw-10@b1d047014c747e84f2a6dd8254a948e16e463bc4 🚀

--- index.3cccd29e.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;fetch("".concat("https://restcountries.com/v2/all?fields=name,capital,currencies","/name/").concat(n,"?fields=name,capital,population,languages,flags")).then((function(n){return n.json()})).then((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.3cccd29e.js.map
--- index.944835aa.js
+++
@@ -1,2 +0,0 @@
-var e;fetch(`https://restcountries.com/v2/all?fields=name,capital,currencies/name/${e}?fields=name,capital,population,languages,flags`).then((e=>e.json())).then((e=>console.log(e)));
-//# sourceMappingURL=index.944835aa.js.map
