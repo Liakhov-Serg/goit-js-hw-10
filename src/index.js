b1d047014c747e84f2a6dd8254a948e16e463bc4
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-11 20:16:39 +0200

    20-16
    111

--- src/index.js
+++ src/index.js
@@ -1,22 +1,58 @@
 import './css/styles.css';
 
 import {fetchCountres}from'./fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-fetchCountres()
-.then((data) => console.log(data));
-// fetch(`https://restcountries.com/v2/all?fields=name,capital,currencies`)
+const refs = {
+    inputElem:document.querySelector(`#search-box`),
+    listElem:document.querySelector(`country-list`),
+    divElem:document.querySelector(`country-info`),
+
+}
+console.log(refs);
+const DEBOUNCE_DELAY = 300;
+
+refs.inputElem.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
+console.log(document.querySelector(`.country-info`));
+function onSearch(e) {
+    // e.preventDefoult();
+}
+
+const name = refs.inputElem.value.trim();
+
+// clearForm();
+// if (name) {
+    fetchCountres(name)
+.then(data => searchCountry(data))
+
+// } else {
+// Notify.failure("Too many matches found. Please enter a more specific name.")
+// }
+console.log(name);
+
 // .then((response) => response.json())
 // .then((data) => console.log(data));
 
-// function createMarkup {
-//     data.forEach((item) => {
-//         const elem = "country-list"
-//     }
+function createMarkup() {
+    data.forEach((item) => {
+        const elem = "country-list"
+    }
 
-//     )
+    )
+}
+
+
+
+
+function searchCountry(countries) {
+    console.log(countries);
+    if (searchCountry.length >= 10) {
+        Notify.success
+    }
+}
+
+
+// if (name.length > 0 && name.trim() !== '') {
+    
 // }
-
-
-const DEBOUNCE_DELAY = 300;
-
-
