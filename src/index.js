e237782e57303744a122d33d77b686c378c9f484
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-11 12:39:52 +0200

    Update index.js
    111

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import './css/styles.css';
-import './fetchCountries';
 
-fetch(`https://restcountries.com/v2/all?fields=name,capital,currencies`)
-.then((response) => response.json())
+import {fetchCountres}from'./fetchCountries';
+
+fetchCountres()
 .then((data) => console.log(data));
+// fetch(`https://restcountries.com/v2/all?fields=name,capital,currencies`)
+// .then((response) => response.json())
+// .then((data) => console.log(data));
+
+// function createMarkup {
+//     data.forEach((item) => {
+//         const elem = "country-list"
+//     }
+
+//     )
+// }
 
 
 const DEBOUNCE_DELAY = 300;
