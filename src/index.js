5e8c9e895b34ef409f4f3c5dfeb9a4dac191ec57
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-12 10:06:20 +0200

    Update index.js
    10-00

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     inputElem:document.querySelector(`#search-box`),
-    listElem:document.querySelector(`country-list`),
-    divElem:document.querySelector(`country-info`),
+    listElem:document.querySelector(`.country-list`),
+    divElem:document.querySelector(`.country-info`),
 
 }
 console.log(refs);
@@ -15,19 +15,38 @@ const DEBOUNCE_DELAY = 300;
 
 refs.inputElem.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
 console.log(document.querySelector(`.country-info`));
+
+
 function onSearch(e) {
-    // e.preventDefoult();
+    e.preventDefoult();
+    const name = refs.inputElem.value.trim();
+    if (name)
+    fetchCountres(name)
+    .then(data => searchCountry(data))
 }
 
-const name = refs.inputElem.value.trim();
+function searchCountry(countries) {
+    console.log(countries);
+    if (searchCountry.length >= 10) {
+        Notify.success
+    } else {
+        renderCountryCard(countries)
+    }
+}
 
-// clearForm();
-// if (name) {
-    fetchCountres(name)
-.then(data => searchCountry(data))
+
+function clearError(countries) {
+    if (countries.length === 0) {
+        throw new Error("404");
+    } else {
+        Notify.failure("Too many matches found. Please enter a more specific name.")
+    }
+}
+//  {
+    
 
 // } else {
-// Notify.failure("Too many matches found. Please enter a more specific name.")
+// 
 // }
 console.log(name);
 
@@ -45,12 +64,24 @@ function createMarkup() {
 
 
 
-function searchCountry(countries) {
-    console.log(countries);
-    if (searchCountry.length >= 10) {
-        Notify.success
-    }
-}
+
+
+function renderCountryCard(countries) {
+    const markupCard = countries.map((({name, capital, population, flags, languages}) => {
+        return `<li class = "country-list_item">
+        <div class = "country-list_title">
+        <img scr = "${flags.svg}" style = "width: 40px; height: 40px"/>
+        <h2>${name}</h2>
+        </div>
+        <p><strong>Capital:</strong> ${capital}</p>
+        <p><strong>Population:</strong> ${population}</p>
+        <p><strong>Languages:</strong> ${Object.values(languages).join(",")
+    }</p>
+        </li>`;
+    }) ).join("");
+    console.log(refs.listElem);
+refs.listElem.insertAdjacentHTML("beforeend", markupCard)
+};
 
 
 // if (name.length > 0 && name.trim() !== '') {
